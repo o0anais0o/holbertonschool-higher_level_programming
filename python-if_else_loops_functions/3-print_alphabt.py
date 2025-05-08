7fced785c1483f8d0d4db10d4c428252da9ddde9
o0anais0o <choisy_anais@hotmail.fr>
2025-05-08 17:30:16 +0200

    Adding comments to the files

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 
 for i in range(97, 123):
+    # Boucle sur tous les entiers de 97 à 122 inclus (car range(97, 123))
+    # Ces entiers correspondent aux codes ASCII des lettres minuscules 'a' à 'z'
     if i not in [101, 113]:
+    # Vérifie que le code ASCII 'i' N'EST PAS 101 (lettre 'e') ni 113 (lettre 'q')
         print("{}".format(chr(i)), end="")
+        # Si la condition est vraie, convertit le code ASCII en caractère et l'affiche sur la même ligne (end="")

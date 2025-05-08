0fb3dc1eddd337781ff321ee66b542084419c470
o0anais0o <choisy_anais@hotmail.fr>
2025-05-08 18:16:19 +0200

    Added comment to 8-uppercase.py file

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -2,12 +2,24 @@
 
 def uppercase(str):
     """Print a string in uppercase followed by a new line."""
-    new_str = ""  # Initialise une chaîne vide pour stocker le résultat
-    for c in str:  # Parcourt chaque caractère de la chaîne
-        # Si le caractère est une minuscule, le convertit en majuscule
+    # Définit une fonction appelée uppercase qui prend un argument str
+    # Docstring : "Affiche une chaîne en majuscules suivie d’un retour à la ligne."
+
+    new_str = ""  
+    # Initialise une chaîne vide new_str qui contiendra le résultat final
+
+    for c in str:  
+        # Parcourt chaque caractère c de la chaîne str
         if ord('a') <= ord(c) <= ord('z'):
+            # Si le caractère c est une lettre minuscule (code ASCII entre 'a' et 'z')
             new_str += chr(ord(c) - 32)
+            # Convertit la lettre minuscule en majuscule :
+            # - ord(c) donne le code ASCII du caractère c
+            # - ord(c) - 32 donne le code ASCII de la lettre majuscule correspondante
+            # - chr(...) convertit le code ASCII en caractère
         else:
-            new_str += c  # Sinon, garde le caractère tel quel
-    # Affiche la chaîne en majuscules, suivie d’une nouvelle ligne
+            new_str += c
+            # Si c n’est pas une minuscule, l’ajoute tel quel à new_str
+
     print("{}".format(new_str))
+    # Affiche la chaîne new_str (en majuscules) suivie d’un retour à la ligne

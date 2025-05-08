cd6921369f11dc1be017bbc8413f48649a46ab2a
o0anais0o <choisy_anais@hotmail.fr>
2025-05-08 17:46:06 +0200

    Editing the 7-islower.py file

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
 
 def islower(c):
-    """Vérifie si un caractère est en minuscule."""
-    # Si c est une chaîne vide ou None, retourne False
-    if not c:
-        return False
-    # Compare le code ASCII du caractère avec les codes ASCII de 'a' et 'z'
-    # Retourne True si c est une lettre minuscule, False sinon
-    return ord('a') <= ord(c) <= ord('z')
+    # Définit une fonction appelée islower qui prend un caractère 'c' en paramètre
+
+    if ord(c) >= 97 and ord(c) <= 122:
+        # ord(c) renvoie le code ASCII du caractère 'c'
+        # Les codes ASCII de 97 à 122 correspondent aux lettres minuscules ('a' à 'z')
+
+        return True
+        # Si 'c' est une lettre minuscule, la fonction retourne True
+
+    return False
+    # Sinon, la fonction retourne False

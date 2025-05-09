f7507aa6fbd0ab0e512ab47c953d98ad25aec533
o0anais0o <choisy_anais@hotmail.fr>
2025-05-09 17:02:16 +0200

    Editing the 3-infinite_add.py file

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -2,10 +2,12 @@
 
 # Ce bloc garantit que le code ne s'exécute que si le script est lancé directement
 if __name__ == "__main__":
-    import sys  # Importe le module sys pour accéder aux arguments de la ligne de commande
+    import sys  # Importe sys pour accéder aux arguments de la ligne de commande
     total = 0  # Initialise la variable qui contiendra la somme des arguments
     # Parcourt tous les arguments passés après le nom du script
     for arg in sys.argv[1:]:
-        total += int(arg)  # Convertit chaque argument en entier et l'ajoute au total
-    print(total)  # Affiche la somme totale suivie d'une nouvelle ligne
+        # Convertit chaque argument en entier et l'ajoute au total
+        total += int(arg)
+    # Affiche la somme totale suivie d'une nouvelle ligne
+    print(total)
     

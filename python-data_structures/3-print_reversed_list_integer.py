1774edaf784d1741a680e335f7e15509c7c80c5f
o0anais0o <choisy_anais@hotmail.fr>
2025-05-12 17:38:50 +0200

    Modificate 3-print_reversed_list_integer.py file

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     """
-    Print all integers in a list in reverse order.
+    Imprimez tous les entiers d'une liste dans l'ordre inverse.
 
     Args:
-        my_list (list): The list of integers to print.
+    my_list (liste) : La liste des entiers à imprimer.
     """
     # Itère de la fin de la liste jusqu'au début
     for i in range(len(my_list) - 1, -1, -1):
-        # Garantit que les entiers sont affichés sans conversion explicite en chaînes
         print("{:d}".format(my_list[i]))

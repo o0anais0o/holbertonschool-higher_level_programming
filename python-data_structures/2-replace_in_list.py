14e0d49ef8510bb2dd88e8d036843085d31c0158
o0anais0o <choisy_anais@hotmail.fr>
2025-05-12 17:07:40 +0200

    Add 2-replace_in_list.py file

---
+++ python-data_structures/2-replace_in_list.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    """
+    Replace an element in a list at a specific position.
+
+    Args:
+        my_list (list): The list in which the element will be replaced.
+        idx (int): The index of the element to replace.
+        element: The new element to insert at the specified index.
+
+    Returns:
+        list: The original list if idx is negative or out of range; 
+              otherwise, the modified list with the new element.
+    """
+    if idx < 0:
+        return my_list
+    
+    if idx >= len(my_list):
+        return my_list
+    
+    my_list[idx] = element
+        
+    return my_list

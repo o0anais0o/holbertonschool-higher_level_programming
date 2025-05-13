4f9df1d2df3aa84f88d46720b3ee4ee9ca02afa2
o0anais0o <choisy_anais@hotmail.fr>
2025-05-13 16:08:09 +0200

    add 6-print_matrix_integer.py file

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    """
+    Affiche une matrice d'entiers ligne par ligne, formatés sans conversion en chaîne.
+    """
+    # Parcourt chaque ligne (sous-liste) de la matrice
+    for row in matrix:
+        # Crée une liste temporaire pour stocker les entiers formatés
+        formatted_row = []
+        # Convertit chaque entier avec str.format() et l'ajoute à la liste
+        for element in row:
+            formatted_row.append("{:d}".format(element))
+        # Affiche la ligne en joignant les éléments par des espaces
+        print(" ".join(formatted_row))

78799431ddffbf41d11ec9bd2257490b4d6feb05
o0anais0o <choisy_anais@hotmail.fr>
2025-05-14 13:34:07 +0200

    edit 4-only_diff_elements.py file

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
     """
-    Retourne un set contenant les éléments présents dans un seul des deux sets.
+    Retourne un set avec les éléments présents dans un seul des deux sets.
     """
-    # Utilise l'opérateur de différence symétrique pour trouver les éléments uniques
+    # Utilise la différence symétrique pour trouver les éléments uniques
     return set_1 ^ set_2

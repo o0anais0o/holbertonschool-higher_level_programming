d6632ea70420f30ada93f500361140d40b819673
o0anais0o <choisy_anais@hotmail.fr>
2025-05-14 15:53:00 +0200

    edit 11-multiply_list_map.py file

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -2,4 +2,3 @@
 def multiply_list_map(my_list=[], number=0):
     """Retourne une nouvelle liste avec toutes les valeurs multipliées par number."""
     return list(map(lambda x: x * number, my_list))
-    

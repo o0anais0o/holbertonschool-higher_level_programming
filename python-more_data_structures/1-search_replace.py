e6e7f1d2683fa44a2eb41b0fa3e839212601a93b
o0anais0o <choisy_anais@hotmail.fr>
2025-05-14 10:31:42 +0200

    add 0-1-search_replace.py file

---
+++ python-more_data_structures/1-search_replace.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    """
+    Retourne une nouvelle liste où toutes les occurrences de 'search'
+    sont remplacées par 'replace'.
+    """
+    # Crée une nouvelle liste en remplaçant les éléments correspondants
+    new_list = []
+    for item in my_list:
+        # Remplace si l'élément correspond à 'search'
+        if item == search:
+            new_list.append(replace)
+        else:
+            new_list.append(item)
+    # Retourne la nouvelle liste
+    return new_list

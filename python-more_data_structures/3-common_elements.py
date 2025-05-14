bce91d9a7caa34b755328cd2e7120b066b332014
o0anais0o <choisy_anais@hotmail.fr>
2025-05-14 11:28:08 +0200

    edit 3-common_elements.py file

---
+++ python-more_data_structures/3-common_elements.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def common_elements(set_1, set_2):
+    """
+    Retourne un set contenant les éléments communs à set_1 et set_2.
+    """
+    # Utilise l'opérateur d'intersection pour trouver les éléments communs
+    return set_1 & set_2

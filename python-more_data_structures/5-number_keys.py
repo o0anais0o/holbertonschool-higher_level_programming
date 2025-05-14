e96499c021933e6073da7762fc071b168a9f5cf9
o0anais0o <choisy_anais@hotmail.fr>
2025-05-14 13:55:30 +0200

    add 5-number_keys.py file

---
+++ python-more_data_structures/5-number_keys.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def number_keys(a_dictionary):
+    """
+    Retourne le nombre de clés dans le dictionnaire a_dictionary.
+    """
+    # Utilise la fonction len() pour compter le nombre de clés
+    return len(a_dictionary)

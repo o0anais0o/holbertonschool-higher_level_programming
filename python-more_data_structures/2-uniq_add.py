2ac4d116bc8ef8bea60cdfdf7c4358dba8ac44d9
o0anais0o <choisy_anais@hotmail.fr>
2025-05-14 11:04:22 +0200

    edit 2-uniq_add.py file

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
     """
-    Additionne tous les entiers uniques d'une liste (chaque entier une seule fois).
+    Additionne tous les entiers uniques d'une liste.
+    Chaque entier n'est compté qu'une seule fois.
     """
     # Utilise un set pour obtenir les éléments uniques de la liste
     unique_elements = set(my_list)

eccebb40eca2eb30a8e94437c385744b07f248dc
o0anais0o <choisy_anais@hotmail.fr>
2025-05-16 14:25:37 +0200

    edit 1-safe_print_integer.py file

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        # Essayer de formater et d'afficher la valeur comme un entier
+        # Try tente d'afficher la valeur comme un entier
         print("{:d}".format(value))
-        # Si les lignes ci-dessus s'exécutent sans erreur,
-        # cela signifie que 'value' a été traitée comme un entier.
+        # Affiche la valeur formatée en entier, suivie d'un retour à la ligne
         return True
-    except (ValueError, TypeError):  # Ou plus généralement "except Exception:"
-        # Si une ValueError (ex: format("hello")) ou TypeError (ex: format([]))
-        # se produit lors du formatage, on arrive ici.
-        # (La question de l'affichage sur stderr reste si sys est interdit)
+        # Retourne True si l'affichage a réussi (la valeur est un entier)
+    except (ValueError, KeyError):
+        # Si la valeur ne peut pas être formatée en entier, retourne False
         return False

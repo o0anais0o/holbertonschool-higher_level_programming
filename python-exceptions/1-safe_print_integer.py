554350f462631ef200667a554ef54fe538279013
o0anais0o <choisy_anais@hotmail.fr>
2025-05-16 14:18:48 +0200

    edit 1-safe_print_integer.py file

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-    # Tente d'afficher la valeur comme un entier
     try:
-        # Affiche la valeur formatée en entier, suivie d'un retour à la ligne
         print("{:d}".format(value))
-        # Retourne True si l'affichage a réussi (la valeur est un entier)
         return True
     except (ValueError, KeyError):
-        # Si la valeur ne peut pas être formatée en entier, retourne False
         return False

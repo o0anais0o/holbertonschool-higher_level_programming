675ddc91d6e6c2fe55f8073ad3e31ebcb8fd95d0
o0anais0o <choisy_anais@hotmail.fr>
2025-05-16 14:30:14 +0200

    edit 2-safe_print_list_integers.py file

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,11 +1,21 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    printed = 0
+    """
+    Affiche les x premiers entiers d'une liste.
+    Args:
+        my_list: liste à parcourir
+        x: nombre d'éléments à traiter
+    Returns:
+        Le nombre d'entiers effectivement affichés.
+    """
+    printed = 0  # Initialise le compteur d'entiers affichés
     for i in range(x):
         try:
+            # Tente d'afficher l'élément à l'indice i comme un entier
             print("{:d}".format(my_list[i]), end="")
-            printed += 1
+            printed += 1  # Incrémente le compteur si l'affichage réussit
         except (ValueError, TypeError):
+            # Ignore l'élément si ce n'est pas un entier
             continue
-    print("")
-    return printed
+    print("")  # Ajoute un retour à la ligne après l'affichage
+    return printed  # Retourne le nombre d'entiers affichés

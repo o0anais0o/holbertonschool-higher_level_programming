c5cbd244548a97e139135077c04847730fd4d718
o0anais0o <choisy_anais@hotmail.fr>
2025-05-16 14:06:52 +0200

    edit 0-safe_print_list.py file

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,21 +1,11 @@
+#!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    """
-    Affiche x éléments d'une liste.
-    Args:
-        my_list: liste dont les éléments seront affichés
-        x: nombre d'éléments à afficher
-    Returns:
-        Le nombre réel d'éléments affichés
-    """
-    count = 0
-    try:
-        # Parcourt les x premiers indices de la liste
-        for i in range(x):
-            # Affiche l'élément sans retour à la ligne
+    elements_printed_count = 0
+    for i in range(x):
+        try:
             print(my_list[i], end="")
-            count += 1  # Incrémente le compteur d'éléments affichés
-    except IndexError:
-        # Arrête la boucle si l'indice dépasse la taille de la liste
-        pass
-    print()  # Ajoute un retour à la ligne après l'affichage
-    return count  # Retourne le nombre d'éléments effectivement affichés
+            elements_printed_count += 1
+        except IndexError:
+            break
+    print()
+    return elements_printed_count

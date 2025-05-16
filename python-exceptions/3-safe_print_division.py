c84ac8734c130f6a9fb153353895fd1e08af6107
o0anais0o <choisy_anais@hotmail.fr>
2025-05-16 14:33:48 +0200

    edit 3-safe_print_division.py file

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,10 +1,21 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
+    """
+    Tente de diviser a par b et affiche le résultat.
+    Args:
+        a: le numérateur
+        b: le dénominateur
+    Returns:
+        Le résultat de la division, ou None si division par zéro.
+    """
     try:
+        # Tente d'effectuer la division
         res = a / b
         return res
     except ZeroDivisionError:
+        # Si division par zéro, assigne None à res
         res = None
         return res
     finally:
+        # Affiche toujours le résultat, même en cas d'erreur
         print("Inside result: {}".format(res))

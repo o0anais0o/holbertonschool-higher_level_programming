e0d5867a0b83b12eb85861202edfab004be0e736
o0anais0o <choisy_anais@hotmail.fr>
2025-05-18 22:21:51 +0200

    Edit 3-say_my_name.py file

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -2,33 +2,31 @@
 """
 Module Name: 3-say_my_name.py
 
-Description: 
-This module includes a function to print 'My name is <first name> 
-<last name>'
+Description:
+This module includes a function to print 'My name is <first name> <last name>'
 
-Features: 
-say_my_name(first_name, last_name=""): Prints 'My name is <first 
-name> <last name>' and returns None
+Features:
+say_my_name(first_name, last_name=""): Prints 'My name is <first name> <last name>' and returns None
 """
 
-def say_my_name(first_name, last_name=""): 
-    """ 
-    say_my_name - prints the string 'My name is <first name> <last 
-    name>' 
 
-    Args: 
-    first_name: string containing my first name. 
-    last_name: string containing my last name. 
+def say_my_name(first_name, last_name=""):
+    """
+    say_my_name - prints the string 'My name is <first name> <last name>'
 
-    Returns: 
-    None 
+    Args:
+        first_name: string containing my first name.
+        last_name: string containing my last name.
 
-    Reasons: 
-    TypeError: if first_name or last_name are not strings 
-    """ 
+    Returns:
+        None
 
-    if not isinstance(first_name, str): 
-        raise TypeError('first_name must be a string') 
-    if not isinstance(last_name, str): 
-        raise TypeError('last_name must be a string') 
+    Raises:
+        TypeError: if first_name or last_name are not strings
+    """
+
+    if not isinstance(first_name, str):
+        raise TypeError('first_name must be a string')
+    if not isinstance(last_name, str):
+        raise TypeError('last_name must be a string')
     print(f"My name is {first_name} {last_name}")

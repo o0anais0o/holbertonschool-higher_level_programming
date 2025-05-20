8927fb62cd0f07894b623f66b4ef3ae247d228cb
o0anais0o <choisy_anais@hotmail.fr>
2025-05-20 13:33:08 +0200

    Add 0-rectangle.py and README.md files

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+Ce module définit une classe Rectangle qui représente une forme rectangulaire.
+"""
+
+
+class Rectangle:
+    """Une classe Rectangle qui définit un rectangle."""
+    pass

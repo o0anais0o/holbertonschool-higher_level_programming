864a2d16ee87d8d14c48068e9d87892e7c74909b
o0anais0o <choisy_anais@hotmail.fr>
2025-05-26 13:46:36 +0200

    Edit 0-lookup.py file

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -3,6 +3,8 @@
 Module 0-lookup
 Fonction lookup qui retourne la liste des attributs et méthodes d’un objet.
 """
+
+
 def lookup(obj):
     """Renvoie la liste des attributs et méthodes disponibles d'un objet."""
     return dir(obj)

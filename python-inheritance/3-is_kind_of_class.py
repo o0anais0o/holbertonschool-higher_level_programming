d36040299530deacdeb38886fd1010a81caab154
o0anais0o <choisy_anais@hotmail.fr>
2025-05-26 16:42:24 +0200

    Edit 3-is_kind_of_class.py file

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+Ce module fournit la fonction is_kind_of_class qui vérifie
+si un objet est une instance d'une classe donnée ou de ses parents.
+"""
+
 
 def is_kind_of_class(obj, a_class):
     """

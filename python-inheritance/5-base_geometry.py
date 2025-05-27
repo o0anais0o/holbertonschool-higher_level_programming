46017edd845cada590b2733fbacc1b2be0896eca
o0anais0o <choisy_anais@hotmail.fr>
2025-05-27 10:16:53 +0200

    Add 5-base_geometry.py file

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+class BaseGeometry:  # Le mot-clé class sert à définir la classe.
+    pass
+    # La déclaration 'pass' permet de créer une classe vide sans contenu,
+    # (aucune méthode ni attribut).

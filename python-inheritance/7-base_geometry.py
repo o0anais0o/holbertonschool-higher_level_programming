f39574c0deafd053d7bb23823bbc61188df23680
o0anais0o <choisy_anais@hotmail.fr>
2025-05-27 14:40:53 +0200

    Edit 7-base_geometry.py file

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 class BaseGeometry:
+    """Classe de base pour la géométrie"""
 
     def area(self):
+        """Lève une exception indiquant que la méthode n'est pas implémentée"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):

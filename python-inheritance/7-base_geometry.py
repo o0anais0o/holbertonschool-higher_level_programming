c7dd26f2c23e650b2db9845b1886542f66e43bb5
o0anais0o <choisy_anais@hotmail.fr>
2025-05-27 14:55:56 +0200

    Edit 7-base_geometry.py file

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -7,6 +7,17 @@ class BaseGeometry:
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
+        """
+        Valide que 'value' est un entier strictement positif.
+
+        Args:
+            name (str): Nom de la variable à valider
+            value (int): Valeur à vérifier
+
+        Raises:
+            TypeError: Si 'value' n'est pas un entier
+            ValueError: Si 'value' est ≤ 0
+        """
         if type(value) is not int:
             raise TypeError(f"{name} must be an integer")
         if value <= 0:

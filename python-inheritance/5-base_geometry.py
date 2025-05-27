d8bc99fa25c0abef04f9d0a79aeaa0ed1966741e
o0anais0o <choisy_anais@hotmail.fr>
2025-05-27 10:26:01 +0200

    Edit 5-base_geometry.py file

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -5,6 +5,7 @@ Ce module définit une classe vide BaseGeometry.
 
 
 class BaseGeometry:  # Le mot-clé class sert à définir la classe.
+    """Classe de base pour la géométrie."""
     pass
     # La déclaration 'pass' permet de créer une classe vide sans contenu,
     # (aucune méthode ni attribut).

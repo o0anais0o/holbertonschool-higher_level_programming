3025997121ef44dcb92b27796473522cac34e337
o0anais0o <choisy_anais@hotmail.fr>
2025-05-28 14:47:10 +0200

    Edit 9-rectangle.py file

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-Définit une classe Rectangle qui hérite de BaseGeometry.
-"""
+""" Définit une classe Rectangle qui hérite de BaseGeometry. """
 
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
@@ -9,10 +7,10 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
     def __init__(self, width, height):
-        self.__width = width
-        self.__height = height
         self.integer_validator("width", width)
         self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
 
     def area(self):
         return self.__width * self.__height

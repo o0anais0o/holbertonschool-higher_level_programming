a957235720954263993503330f79a8512f304fe4
o0anais0o <choisy_anais@hotmail.fr>
2025-06-02 11:25:16 +0200

    Add 1-write_file.py file

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    """
+    with open(filename, "w") as f:
+        f.write(text)("Bonjour, monde !")
+        len(text)

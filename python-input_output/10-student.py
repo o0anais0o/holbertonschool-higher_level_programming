930a8d9b854f7505d433276f2578a59d753869d3
o0anais0o <choisy_anais@hotmail.fr>
2025-06-08 12:49:39 +0200

    Edit 10-student.py file

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 class Student:
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
@@ -8,4 +10,5 @@ class Student:
         if attrs is None:
             return self.__dict__
         else:
-            return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
+            return {attr: getattr(self, attr)
+                    for attr in attrs if hasattr(self, attr)}

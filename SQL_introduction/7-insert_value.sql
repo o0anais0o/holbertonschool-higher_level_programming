fb4d25252b02bcad782f46035a293a633f05fdc9
o0anais0o <choisy_anais@hotmail.fr>
2025-06-24 09:40:53 +0200

    Modification du texte de la ligne insérée dans la table first_table pour corriger la traduction de 'Meilleure école' à 'Best School'

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
 -- Insère une nouvelle ligne dans la table first_table (id = 89, name = 'Meilleure école')
-INSERT INTO first_table (id, name) VALUES (89, 'Meilleure école');
+INSERT INTO first_table (id, name) VALUES (89, 'Best School');

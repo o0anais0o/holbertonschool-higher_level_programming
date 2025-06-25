34859d8e1624f7decc8c54bc149df7cbb2f14348
o0anais0o <choisy_anais@hotmail.fr>
2025-06-25 16:10:46 +0200

    Ajout de la requête SQL pour répertorier toutes les villes de Californie dans la base de données hbtn_0d_usa

---
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,9 @@
+-- Répertorie toutes les villes de Californie qui peuvent être trouvées dans la base de données hbtn_0d_usa
+SELECT cities.id, cities.name
+FROM cities
+WHERE cities.state_id = (
+    SELECT states.id
+    FROM states
+    WHERE states.name = 'California'
+)
+ORDER BY cities.id;

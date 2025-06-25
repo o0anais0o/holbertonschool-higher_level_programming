12de8946f1723043fe30c889144dacc8a9bb6671
o0anais0o <choisy_anais@hotmail.fr>
2025-06-25 16:50:38 +0200

    Ajout de la requête SQL pour répertorier toutes les émissions avec leur genre_id

---
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,5 @@
+-- Répertorie toutes les émissions dans hbtn_0d_tvshows avec leur genre_id (NULL si aucun genre)
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;

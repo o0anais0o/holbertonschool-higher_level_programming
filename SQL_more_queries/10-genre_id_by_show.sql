2f9faff114787f583a7aff3dd1a5bfc455d95b42
o0anais0o <choisy_anais@hotmail.fr>
2025-06-25 16:32:12 +0200

    Ajout de la requête SQL pour répertorier toutes les émissions avec au moins un genre lié

---
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,5 @@
+-- Répertorie toutes les émissions avec au moins un genre lié, en affichant le titre et le genre_id
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;

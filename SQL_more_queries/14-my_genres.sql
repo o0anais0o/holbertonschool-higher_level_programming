734fc9aed964de9f195c1cb0ee9af8f3f173cb0f
o0anais0o <choisy_anais@hotmail.fr>
2025-06-27 09:53:38 +0200

    Suppression de l'instruction USE dans la requête SQL pour répertorier les genres de la série Dexter

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,7 +1,6 @@
 -- Répertorie tous les genres de la série Dexter à partir de la base de données passée en argument.
 -- Chaque enregistrement affiche : tv_genres.name
 -- Les résultats sont triés par ordre croissant selon le nom du genre.
-USE hbtn_0d_tvshows;
 SELECT tv_genres.name
 FROM tv_shows
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id

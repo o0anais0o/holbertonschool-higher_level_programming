684aa3f69b16dd7ca1f5aeff972e299f9e4e4c27
o0anais0o <choisy_anais@hotmail.fr>
2025-07-14 13:34:35 +0200

    Add `.red` class

--- javascript-dom_manipulation/2-script.js
+++ javascript-dom_manipulation/2-script.js
@@ -1,3 +1,9 @@
 document.querySelector('#red_header').addEventListener('click', function () {
   document.querySelector('header').classList.add('red');
 });
+
+/* Explications :
+On sélectionne l’élément avec l’id red_header.
+Lors d’un clic, on ajoute la classe red à la balise <header>.
+La classe .red est déjà définie dans le <style> de la page pour appliquer la couleur rouge.
+Instructions : Ouvrir 2-main.html dans le navigateur. Cliquer sur « Red header » : le texte du <header> devient rouge. */

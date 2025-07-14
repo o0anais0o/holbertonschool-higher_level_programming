9cdaa9f1cfd1fdf2b1e4c037430d0cd844ed9fb6
o0anais0o <choisy_anais@hotmail.fr>
2025-07-14 13:37:55 +0200

    List of elements

--- javascript-dom_manipulation/4-script.js
+++ javascript-dom_manipulation/4-script.js
@@ -4,3 +4,14 @@ document.querySelector('#add_item').addEventListener('click', function () {
   li.textContent = 'Item';
   ul.appendChild(li);
 });
+
+/* 
+Explications :
+On sélectionne l’élément avec l’id add_item.
+Lors d’un clic, on sélectionne la liste <ul class="my_list">.
+On crée un nouvel élément <li>, on lui donne le texte « Item », puis on l’ajoute à la liste.
+
+Instructions : 
+Ouvrir 4-main.html dans le navigateur.
+Cliquer sur « Add item » : un nouvel élément <li>Item</li> est ajouté à la liste à chaque clic.
+*/

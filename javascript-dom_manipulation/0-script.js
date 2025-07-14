6ab7239eecfe04de6e529599d43411067db00b51
o0anais0o <choisy_anais@hotmail.fr>
2025-07-14 13:39:39 +0200

    Color Me

--- javascript-dom_manipulation/0-script.js
+++ javascript-dom_manipulation/0-script.js
@@ -6,5 +6,6 @@ document.querySelector('header') sélectionne la balise <header> de la page.
 .style.color = '#FF0000' change sa couleur de texte en rouge.
 
 Instructions : 
-Ouvrir le fichier 0-main.html dans le navigateur. Le texte de l’en-tête doit apparaître en rouge.
+Ouvrir le fichier 0-main.html dans le navigateur. 
+Le texte de l’en-tête doit apparaître en rouge.
 */

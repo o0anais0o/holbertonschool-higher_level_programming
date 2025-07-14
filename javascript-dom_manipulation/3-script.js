fcc5676489cb3ca0a2684c3b52decc151aeed6ea
o0anais0o <choisy_anais@hotmail.fr>
2025-07-14 13:36:20 +0200

    Toggle classes

--- javascript-dom_manipulation/3-script.js
+++ javascript-dom_manipulation/3-script.js
@@ -1,9 +1,3 @@
-/* Explications :
-On sélectionne le bouton avec l’id toggle_header.
-Au clic, on sélectionne le <header>.
-Si le header a la classe red, on la remplace par green.
-Sinon (donc s’il a green), on la remplace par red.
-Le header aura toujours soit la classe red soit la classe green, jamais les deux, jamais aucune.*/
 document.querySelector('#toggle_header').addEventListener('click', function () {
   const header = document.querySelector('header');
   if (header.classList.contains('red')) {
@@ -15,3 +9,11 @@ document.querySelector('#toggle_header').addEventListener('click', function () {
   }
 });
 
+/* Explications :
+On sélectionne le bouton avec l’id toggle_header.
+Au clic, on sélectionne le <header>.
+Si le header a la classe red, on la remplace par green.
+Sinon (donc s’il a green), on la remplace par red.
+Le header aura toujours soit la classe red soit la classe green, jamais les deux, jamais aucune.
+Instructions : Ouvrir 3-main.html dans le navigateur. 
+Cliquer sur « Toggle header » : la couleur du header alterne entre rouge et vert à chaque clic.*/
